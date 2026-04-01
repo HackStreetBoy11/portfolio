5ef720fd95fd7e4b974261aad59ae6aba73cc9dc
Varun <varun@gmail.com>
2026-04-01 15:53:38 +0530

    Updated portfolio: added new projects and images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,5 +1,29 @@
 export const projects = [
     {
+    title: "AQIPrediction",
+    description: "AQI Prediction Live Demo ↗. Built a Decision Tree Regression model to forecast air quality index (AQI) using historical data. Deployed via Flask for interactive, real-time predictions and analysis.",
+    link: "https://aqiprediction-q6vq.onrender.com/",
+    github: "https://github.com/HackStreetBoy11/AqiPrediction",
+    image: "/images/aqi.jpg",
+    technologies: ["Python", "Flask", "Decision Tree Regression", "Scikit-learn", "NumPy", "SciPy"]
+},
+{
+title: "MovieRecommendationSystem",
+description: "Movie Recommendation System Live Demo ↗. Built a content-based recommender using NLP, TF-IDF, and cosine similarity, achieving 92% accuracy on 5,000+ movies. Deployed via FastAPI for fast, scalable recommendations.",
+link: "https://movierecommendation-juspzbc78f7k2eypqxj8gr.streamlit.app/",
+github: "https://github.com/HackStreetBoy11/MovieRecommendation",
+image: "/images/moviesR.jpg",
+technologies: ["Python", "FastAPI", "NLP", "TF-IDF", "Cosine Similarity", "Scikit-learn", "Pandas", "NumPy"]
+},
+{
+title: "Heart Stroke Prediction",
+description: " Built a predictive model using K-Nearest Neighbors (KNN) on a heart health dataset to estimate stroke risk. Deployed via Streamlit for interactive, real-time predictions.",
+link: "https://heartratepridiction2002.streamlit.app/",
+github: "https://github.com/HackStreetBoy11/HeartAttactRisk",
+image: "/images/Heart.jpg",
+technologies: ["Python", "KNN", "Scikit-learn", "Pandas", "NumPy", "Streamlit"]
+},
+    {
         title: "Nutrify - Food Tracking App",
         description:
             "A calorie and nutrition tracking web app built using Next.js, Convex, Clerk, and OpenAI for AI-powered food insights.",
@@ -25,8 +49,5 @@ export const projects = [
         github: "https://github.com/HackStreetBoy11/movie_finder",
         image: "/images/movies.png",
         technologies: ["Vite", "React", "JavaScript", "Tailwind CSS", "Appwrite", "TMDB API"]
-
-
-
-    }
+    }  
 ];
